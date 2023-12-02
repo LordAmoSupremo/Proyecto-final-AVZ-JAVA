efb52c85a2add3f45dc78016eff7e3aa84ca21d9
LordAmoSupremo <125117427+LordAmoSupremo@users.noreply.github.com>
2023-12-01 23:20:52 -0600

    Add files via upload

---
+++ eviFinal/src/main/java/com/example/evifinal/EviFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.evifinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EviFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EviFinalApplication.class, args);
+    }
+
+}
